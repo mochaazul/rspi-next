5acbd5e4fd0a177a38eff68d3551bc0ad974ab42
Fadly Kayo <fadly.kayo@rebelworks.co>
2023-11-06 18:44:32 +0700

    [Package] Init package json, add some folder pages

---
+++ src/app/[locale]/(main)/reset-password/useResetPassword.ts
@@ -0,0 +1,81 @@
+import { updatePassword as updatePasswordAction } from 'stores/actions';
+import { useAppDispatch } from 'hooks';
+import {
+	requiredRule,
+	minLengthRule,
+	createFieldConfig,
+	emailRule
+} from 'helpers';
+import { UpdatePasswordType } from 'interface';
+import { Languages } from 'constant';
+import { navigation } from 'helpers';
+
+import { removeUser as removeUserData } from 'stores/User';
+
+const {
+	resetForm: {
+		newPasswordConfirmationLabel,
+		newPasswordLabel,
+		oldPasswordLabel,
+	}
+} = Languages.page.resetPassword;
+
+export const resetPasswordField = {
+	old_password: {
+		...createFieldConfig({
+			name: 'old_password',
+			type: 'password'
+		}),
+		validationRules: [
+			requiredRule('password'),
+			minLengthRule('password', 8)
+		],
+		label: oldPasswordLabel
+	},
+	new_password: {
+		...createFieldConfig({
+			name: 'new_password',
+			type: 'password'
+		}),
+		validationRules: [
+			requiredRule('password'),
+			minLengthRule('password', 8)
+		],
+		label: newPasswordLabel
+	},
+	confirm_password: {
+		...createFieldConfig({
+			name: 'confirm_password',
+			type: 'password'
+		}),
+		validationRules: [
+			requiredRule('password'),
+			minLengthRule('password', 8)
+		],
+		label: newPasswordConfirmationLabel
+	},
+};
+
+
+const useResetPassword = () => {
+	const { navigate } = navigation();
+	const removeUser = useAppDispatch(removeUserData);
+	const resetPassword = useAppDispatch<UpdatePasswordType>(updatePasswordAction);
+	const onClickResetPassword = async ({ confirm_password, new_password }: UpdatePasswordType) => {
+		await resetPassword({
+			payload: {
+				confirm_password,
+				new_password
+			}
+		});
+		removeUser();
+		navigate('/');
+	};
+
+	return {
+		onClickResetPassword,
+		resetPasswordField
+	};
+};
+
+export default useResetPassword;
