4b1e55dab9b5f746e89da1dd44a426187df47876
gifaniasofia <gifaniasofia@gmail.com>
2023-11-09 15:25:48 +0700

    remove svgr

--- next.config.js
+++ next.config.js
@@ -12,30 +12,7 @@ const nextConfig = {
         hostname: "**",
       },
     ],
-  },
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     loader: '@svgr/webpack',
-  //     options: {
-  //       prettier: false,
-  //       svgo: true,
-  //       svgoConfig: {
-  //         plugins: [
-  //           {
-  //             name: 'preset-default',
-  //             params: {
-  //               overrides: { removeViewBox: false },
-  //             },
-  //           },
-  //         ],
-  //       },
-  //       titleProp: true,
-  //     },
-  //     test: /\.svg$/,
-  //   });
-
-  //   return config;
-  // },
+  }
 };
 
 module.exports = nextConfig;
