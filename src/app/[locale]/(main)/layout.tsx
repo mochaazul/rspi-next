bc741cc48368b2eb29cab6c80109569480c4f1b9
galesaputra <revolveheld@gmail.com>
2023-11-21 11:36:39 +0700

    feat: navigate to hospital detail in header

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -31,47 +31,48 @@ export default async function RootLayout({
 	props,
 	children,
 }: {
-  children: React.ReactNode,
-  props: {
-    containerStyle?: OutletStyleType;
-    footerShow?: boolean;
-  };
+	children: React.ReactNode,
+	props: {
+		containerStyle?: OutletStyleType;
+		footerShow?: boolean;
+	};
 }) {
-  
+
 	const headersList = headers();
-	
+
 	const pathname = headersList.get('x-invoke-path') || '';
 
 	const shouldRenderReminder = !blacklistedRoute.some(route => pathname.includes(route));
-	
+
 	const hospitals = await hospitalsFetch();
 	const footers = await footersFetch();
 	const centerOfExcellence = await centerOfExcellenceFetch();
 	const facilityServices = await facilityServicesFetch();
 	const notificationResponse = await notificationResponseFetch();
-  
+
 	return (
 		<>
 			<Header
-				hospitalData = { hospitals.data }
-				centerOfExcellenceData = { centerOfExcellence.data }
-				facilityServicesData = { facilityServices.data }
-				notificationResponseData = { notificationResponse?.data }
-				marAllReadNotifFunc = { marAllReadNotif }
+				hospitalData={ hospitals.data }
+				centerOfExcellenceData={ centerOfExcellence.data }
+				facilityServicesData={ facilityServices.data }
+				notificationResponseData={ notificationResponse?.data }
+				marAllReadNotifFunc={ marAllReadNotif }
+				footersData={ footers.data }
 			/>
 			{ children }
 
 			{ props?.footerShow !== false &&
-        <Footer footerData = { footers.data } />
+				<Footer footerData={ footers.data } />
 			}
 			{ props?.footerShow !== false &&
-        <CallForAmbulance hospitalData = { hospitals.data } />
+				<CallForAmbulance hospitalData={ hospitals.data } />
 			}
 			{ appStage !== 'prod' &&
-        <DevTools />
+				<DevTools />
 			}
 			{ shouldRenderReminder &&
-        <MedicalRecordReminder />
+				<MedicalRecordReminder />
 			}
 		</>
 	);
