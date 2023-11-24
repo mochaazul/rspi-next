6cac02d001e2d1f3ce03763875651b02d5607003
Mochamad Zulfikar <M.zulfikar@rebelworks.co>
2023-11-24 13:49:28 +0700

    update to ver 147-next

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@
 const environment = {
 	dev: {
 		baseUrl: process.env.NEXT_PUBLIC_BASE_URL_DEV,
-		version: '1.1.145-next'
+		version: '1.1.147-next'
 	},
 	staging: {
 		baseUrl: process.env.NEXT_PUBLIC_BASE_URL_STAGING,
