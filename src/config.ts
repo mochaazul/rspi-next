d7e1ccb16ef94e2d88ddcda637e092277fa04802
galesaputra <revolveheld@gmail.com>
2023-11-24 21:37:43 +0700

    update config to 1.1.152-next

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@
 const environment = {
 	dev: {
 		baseUrl: process.env.NEXT_PUBLIC_BASE_URL_DEV,
-		version: '1.1.151-next'
+		version: '1.1.152-next'
 	},
 	staging: {
 		baseUrl: process.env.NEXT_PUBLIC_BASE_URL_STAGING,
