e0069abdeb552a255a7689cbc29bf617c9950feb
gifaniasofia <gifaniasofia@gmail.com>
2023-11-28 16:48:22 +0700

    [Header] fixing slicing header, mainnavlanguage, and languageselector. add submenu & mainnavlanguage on mobile sidebar

--- src/components/ui/LanguageSelector/index.tsx
+++ src/components/ui/LanguageSelector/index.tsx
@@ -14,14 +14,14 @@ const languageItem = [
 		label: 'ID',
 		value: 'id',
 		longLabel: 'Bahasa Indonesia',
-		icon: <Image src='/images/ic/flag.svg' alt='Indonesia Lang Icon' width={ 20 } height={ 20 }/>
+		icon: <Image src='/images/ic/flag.svg' alt='Indonesia Lang Icon' width={ 20 } height={ 20 } />
 	},
 	{
 		key: 'en',
 		label: 'EN',
 		value: 'en',
 		longLabel: 'English',
-		icon: <Image src='/images/ic/eng-flag.svg' alt='English Lang Icon' width={ 20 } height={ 20 }/>
+		icon: <Image src='/images/ic/eng-flag.svg' alt='English Lang Icon' width={ 20 } height={ 20 } />
 	}
 ];
 
@@ -38,42 +38,36 @@ export const LanguageSelector = () => {
 	};
 
 	return (
-		<>
-			<div className='flex h-[21px]'>
+		<div className='relative flex items-center'>
+			<div className='flex items-center gap-2 lg:gap-3 cursor-pointer' onClick={ () => setShowLanguagePicker(!showLanguagePicker) }>
 				{ languageItem.find(item => currentLang === item.value)?.icon }
-			</div>
-			<div className='flex items-center pl-[20px] gap-[5px] cursor-pointer' onClick={ () => setShowLanguagePicker(!showLanguagePicker) }>
-				<Text fontSize='16px' fontWeight='900' color='white'>
-					{ languageItem.find(item => currentLang === item.value)?.label }
-				</Text>
-				<icons.LightArrowDown alt='' />
-				<div className='absolute right-0 top-10'>
-					<Picker show={ showLanguagePicker } className='!mt-0 !shadow-[0px_4px_10px_0px_rgba(0,0,0,0.15)]'>
-						{
-							languageItem.map((item, index) => (
-								<div
-									key={ index }
-									className={ `flex cursor-pointer gap-4 p-5 ${ index > 0 ? 'border-t border-[#F0F2F9]' : '' }` }
-									onClick={ () => onChangeLanguage(item.value) }
-								>
-									<>
-										{ item.icon }
-										<Text
-											fontSize='16px'
-											fontWeight='700'
-											lineHeight='19px'
-											color={ item.value === currentLang ? colors.green.brandAccent : colors.black.general }
-											text={ item.longLabel }
-										/>
-									</>
-									<Icons.Check className={ `check-icon ${ item.value === currentLang ? '' : 'hidden' }` } size={ 20 } />
-								</div>
-							))
-						}
-					</Picker>
+				<div className='flex items-center gap-x-[5px]'>
+					<Text fontSize='16px' fontWeight='900' color='white'>
+						{ languageItem.find(item => currentLang === item.value)?.label }
+					</Text>
+					<icons.LightArrowDown />
 				</div>
 			</div>
-		</>
+			<Picker show={ showLanguagePicker } className='max-lg:min-w-[165px] !mt-0 !mr-0 !shadow-[0px_4px_10px_0px_rgba(0,0,0,0.15)] left-0 lg:left-auto lg:!right-0 top-7 w-full'>
+				{
+					languageItem.map((item, index) => (
+						<div
+							key={ index }
+							className={ `flex cursor-pointer py-[15px] lg:py-5 px-5 ${ index > 0 ? 'border-t border-[#F0F2F9]' : '' }` }
+							onClick={ () => onChangeLanguage(item.value) }
+						>
+							<Text
+								fontSize='16px'
+								fontWeight='700'
+								lineHeight='19px'
+								color={ item.value === currentLang ? colors.green.brandAccent : colors.black.general }
+								text={ item.longLabel }
+							/>
+						</div>
+					))
+				}
+			</Picker>
+		</div>
 	);
 };
 
