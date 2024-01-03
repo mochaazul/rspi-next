ca3671ced014b187e858a9fc8a9712e659326570
M. Zulfikar <mochaa.zulfikar@gmail.com>
2024-01-03 13:32:09 +0700

    update to 1.1.178-next

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@
 const environment = {
 	dev: {
 		baseUrl: process.env.NEXT_PUBLIC_BASE_URL_DEV,
-		version: '1.1.177-next'
+		version: '1.1.178-next'
 	},
 	staging: {
 		baseUrl: process.env.NEXT_PUBLIC_BASE_URL_STAGING,
