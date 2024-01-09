e2cf5816ba2017d4b8056818fabd82206115336a
M. Zulfikar <mochaa.zulfikar@gmail.com>
2024-01-09 10:01:29 +0700

    update to 1.1.184-next

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@
 const environment = {
 	dev: {
 		baseUrl: process.env.NEXT_PUBLIC_BASE_URL_DEV,
-		version: '1.1.183-hotfix2-next'
+		version: '1.1.184-next'
 	},
 	staging: {
 		baseUrl: process.env.NEXT_PUBLIC_BASE_URL_STAGING,
