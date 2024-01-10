bf367020ef5b6e220cb1f6fde502515c513a339a
gifaniasofia <gifaniasofia@gmail.com>
2024-01-10 14:10:18 +0700

    [FW 396 PatientPortal] tidy up

--- src/app/[locale]/(main)/page-wrapper.tsx
+++ src/app/[locale]/(main)/page-wrapper.tsx
@@ -23,50 +23,50 @@ export default async function PageWrapper({
 	children: React.ReactNode,
 }) {
 	const session = await getSession();
-	// const hospitals = await hospitalsFetch();
-	// const footers = await footersFetch();
-	// const centerOfExcellence = await centerOfExcellenceFetch();
-	// const facilityServices = await facilityServicesFetch();
-	// const footerData = footers.data?.map(footer => ({
-	// 	title: footer.title,
-	// 	slug: footer.slug,
-	// 	footer_category: footer.footer_category
-	// }));
-	// const hospitalData = hospitals.data?.map(hospital => ({
-	// 	slug: hospital.slug,
-	// 	img_url: hospital.img_url,
-	// 	name: hospital.name,
-	// 	address: hospital.address,
-	// 	phone: hospital.phone,
-	// 	id: hospital.id,
-	// 	hospital_code: hospital.hospital_code
-	// }));
-	// const centerOfExcellenceData = centerOfExcellence?.data?.map(coe => ({
-	// 	img_url: coe.img_url,
-	// 	slug: coe.slug,
-	// 	title: coe.title
-	// }));
-	// const facilityServicesData = facilityServices?.data?.map(facility => ({
-	// 	slug: facility.slug,
-	// 	image_url: facility.image_url,
-	// 	name: facility.name
-	// }));
+	const hospitals = await hospitalsFetch();
+	const footers = await footersFetch();
+	const centerOfExcellence = await centerOfExcellenceFetch();
+	const facilityServices = await facilityServicesFetch();
+	const footerData = footers.data?.map(footer => ({
+		title: footer.title,
+		slug: footer.slug,
+		footer_category: footer.footer_category
+	}));
+	const hospitalData = hospitals.data?.map(hospital => ({
+		slug: hospital.slug,
+		img_url: hospital.img_url,
+		name: hospital.name,
+		address: hospital.address,
+		phone: hospital.phone,
+		id: hospital.id,
+		hospital_code: hospital.hospital_code
+	}));
+	const centerOfExcellenceData = centerOfExcellence?.data?.map(coe => ({
+		img_url: coe.img_url,
+		slug: coe.slug,
+		title: coe.title
+	}));
+	const facilityServicesData = facilityServices?.data?.map(facility => ({
+		slug: facility.slug,
+		image_url: facility.image_url,
+		name: facility.name
+	}));
 
 	return (
 		<>
-			{/* <Header
+			<Header
 				session={ session }
 				hospitalData={ hospitalData }
 				centerOfExcellenceData={ centerOfExcellenceData }
 				facilityServicesData={ facilityServicesData }
 				marAllReadNotifFunc={ marAllReadNotif }
-			/> */}
+			/>
 			{ children }
-			{/* <Footer
+			<Footer
 				footerData={ footerData }
 				hospitalData={ hospitalData }
-			/> */}
-			{/* <CallForAmbulance hospitalData={ hospitalData } session={ session } /> */ }
+			/>
+			<CallForAmbulance hospitalData={ hospitalData } session={ session } />
 			{
 				appStage !== 'prod' &&
 				<DevTools />
