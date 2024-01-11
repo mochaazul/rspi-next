8614452d8ae240f22a6660ebcb70e4f06e5fc922
Muhammad Khoirul Bakhtiar <khoirul.bakhtiar@rebelworks.co>
2024-01-11 16:29:07 +0700

    fixing mobile news detail related article

--- src/components/ui/PageComponents/News/NewsDetail/index.tsx
+++ src/components/ui/PageComponents/News/NewsDetail/index.tsx
@@ -27,56 +27,20 @@ const NewsDetail = async({
 	const currentLang = getCurrentLocale();
 	const renderNews = () => {
 		return (
-			<div className='max-sm:mx-[15px] '>
-				<Text fontType='p' fontWeight='700' fontSize='20px' lineHeight='30px'>
-					{ filteredSelectedArticle?.short_description }
-				</Text>
-				<img alt={ filteredSelectedArticle?.title } src={ filteredSelectedArticle?.img_url } className='mx-auto my-[50px] lg:w-[729px] lg:h-[502px] object-cover rounded-[5px]' />
-
-				<TextHtml
-					style={ { color: colors.grey.dark } }
-					className='innerHTML mt-[10px]'
-					htmlStr={ filteredSelectedArticle?.content ?? '' }
-				/>
-
-				<div className={ filteredSelectedArticle?.category === 'healthfirst' ? 'hidden' : 'sm:hidden' }>
-					
-					<span className='text-gray-1 font-black w-auto text-[16px] py-[5px] sm:border-b-[4px] border-b-[3px] border-green-secondary'>
-						{ t('relatedNews') }
-					</span>
-
-					<div className='divide-y divide-solid pt-[10px]'>
-						{ Object.values(relatedNews || []).map((a, index) => {
-							return (
-								<div key={ index }>
-									<Link href={ `/news/${a.slug}` }>
-										<Text
-											text={ a.posted_date }
-											className='py-[10px]'
-											fontSize='12px'
-											fontWeight='400'
-											lineHeight='14px'
-											color={ colors.grey.dark }
-											fontType='p'
-										/>
-										<Text
-											fontType='h3'
-											text={ a.title }
-											className='pb-[10px]'
-											fontSize='14px'
-											fontWeight='900'
-											lineHeight='24px'
-											color={ colors.grey.darker }
-										/>
-									</Link>
-								</div>
-							);
-						}) }
-					</div>
+			<div>
+				<div className='max-sm:mx-[15px] '>
+					<Text fontType='p' fontWeight='700' fontSize='20px' lineHeight='30px'>
+						{ filteredSelectedArticle?.short_description }
+					</Text>
+					<img alt={ filteredSelectedArticle?.title } src={ filteredSelectedArticle?.img_url } className='mx-auto my-[50px] lg:w-[729px] lg:h-[502px] object-cover' />
+				
+					<TextHtml
+						className='innerHTML mt-[10px] '
+						htmlStr={ filteredSelectedArticle?.content ?? '' }
+					/>
 				</div>
 				{
-					relatedNews.length > 0 || specialty.length > 0
-						?
+					relatedNews.length > 0 || specialty.length > 0 ?
 						<div className='bg-[#FAFAFA] max-sm:px-[15px] max-sm:py-[24px] max-sm:mt-[12px]'>
 							<div className={ filteredSelectedArticle?.category === 'healthfirst' || relatedNews.length === 0 ? 'hidden' : 'sm:hidden' }>
 								<span className='text-gray-1 font-black w-auto text-[16px] py-[5px] sm:border-b-[4px] border-b-[3px] border-green-secondary'>
@@ -86,7 +50,7 @@ const NewsDetail = async({
 									{ Object.values(relatedNews || []).map((a, index) => {
 										return (
 											<div key={ index }>
-												<Link href={ `/news/${a.slug}` }>
+												<Link href={ `/news/${ a.slug }` }>
 													<Text
 														text={ dayjs(a?.posted_date).locale(currentLang)
 															.format('dddd, DD MMMM YYYY') }
@@ -224,41 +188,9 @@ const NewsDetail = async({
 							<SocialShare date={ filteredSelectedArticle.posted_date } />
 						</div >
 						<div className='content-wrapper flex mt-[50px] mb-[100px] max-sm:mt-[25px]'>
-							<div className={ ` ${filteredSelectedArticle?.category === 'healthfirst' ? 'w-full' : 'w-[729px]'} leftSide mt-0 ` }>
+							<div className={ ` ${ filteredSelectedArticle?.category === 'healthfirst' ? 'w-full' : 'w-[729px]' } leftSide mt-0 ` }>
 								{ filteredSelectedArticle?.category === 'healthfirst' ? renderHealthFirst() : renderNews() }
-								<div className={ filteredSelectedArticle?.category === 'healthfirst' ? 'hidden' : 'sm:hidden' } >
-
-									<span className='text-gray-1 font-black w-auto text-[16px] py-[5px] sm:border-b-[4px] border-b-[3px] border-green-secondary'>
-										{ t('specialty') }
-									</span>
-									<div className='divide-y divide-solid pt-[10px]'>
-										{ Object.values(specialty || [])?.map((specialty, index) => {
-											return (
-												<div key={ index }>
-													<Text
-														fontType='p'
-														text={ specialty.fullname_doctor }
-														className='py-[10px]'
-														fontSize='14px'
-														fontWeight='900'
-														lineHeight='24px'
-														color={ colors.grey.darker }
-													/>
-													<Text
-														fontType='p'
-														text={ specialty.speciality }
-														className='pb-[10px]'
-														fontSize='12px'
-														fontWeight='400'
-														lineHeight='14px'
-														color={ colors.grey.dark }
-													/>
-												</div>
-											);
-										}) }
-									</div>
-								</div>
-							</div >
+							</div>
 							{ /* Dekstop View */ }
 							< div className={ ` ${filteredSelectedArticle?.category === 'healthfirst' ? 'hidden' : 'rightSide sm:ml-[32px] max-sm:hidden mr-auto w-[349px] '} ` }>
 								<div className={ filteredSelectedArticle?.category === 'healthfirst' ? 'hidden' : 'w-[349px]' }>
