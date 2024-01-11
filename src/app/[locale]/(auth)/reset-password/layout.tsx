d658dfb894a591e9ac00f05b83f35d9956adf26a
M. Zulfikar <mochaa.zulfikar@gmail.com>
2024-01-11 21:36:45 +0700

    add lang wrappers

--- src/app/[locale]/(auth)/reset-password/layout.tsx
+++ src/app/[locale]/(auth)/reset-password/layout.tsx
@@ -9,6 +9,7 @@ import {
 	facilityServicesFetch,
 	hospitalsFetch,
 } from '../../(main)/helpers';
+import LangWrapper from '@/components/ui/LangWrapper';
 
 export default async function AuthLayout({ children }: PropsWithChildren) {
 	const hospitals = await hospitalsFetch();
@@ -24,7 +25,9 @@ export default async function AuthLayout({ children }: PropsWithChildren) {
 				className='md:hidden'
 			/>
 
-			{ children }
+			<LangWrapper>
+				{ children }
+			</LangWrapper>
 		</>
 	);
 }
